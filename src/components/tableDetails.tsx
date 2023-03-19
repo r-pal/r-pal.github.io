38427c60b76b3affd051945d5328dc535ec2cb65
r-pal <90906037+r-pal@users.noreply.github.com>
2023-03-19 11:56:47 +0000

    Table

--- src/components/tableDetails.tsx
+++ src/components/tableDetails.tsx
@@ -1,33 +1,33 @@
 export type Column = {
-    header: string;
-    id: string;
-    menu: string;
-    content: string;
-}
+  header: string;
+  id: string;
+  menu: string;
+  content: string;
+};
 
 export const tableColumns: Column[] = [
-    {
-        header: "Name",
-        id: "name",
-        menu: "Basic Information",
-        content: "content"
-    },
-    {
-        header: "Diameter",
-        id: "diameter",
-        menu: "Basic Information",
-        content: "content"
-    },
-    {
-        header: "Colour",
-        id: "colour",
-        menu: "Style information",
-        content: "content"
-    },
-    {
-        header: "Abstract?",
-        id: "abstract",
-        menu: "Miscellaneous",
-        content: "content"
-    }
-]
+  {
+    header: "Name",
+    id: "name",
+    menu: "Basic Information",
+    content: "content",
+  },
+  {
+    header: "Diameter",
+    id: "diameter",
+    menu: "Basic Information",
+    content: "content",
+  },
+  {
+    header: "Colour",
+    id: "colour",
+    menu: "Style information",
+    content: "content",
+  },
+  {
+    header: "Abstract?",
+    id: "abstract",
+    menu: "Miscellaneous",
+    content: "content",
+  },
+];
