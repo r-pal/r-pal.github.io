14392e5a9db3e4c696ad84187b7157c4b6ea3646
r-pal <90906037+r-pal@users.noreply.github.com>
2023-03-19 16:46:24 +0000

    xGates in operation, colour fill click

--- src/components/Level02.tsx
+++ src/components/Level02.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { P5CanvasInstance, ReactP5Wrapper } from "react-p5-wrapper";
 import { canvasHeight, canvasWidth } from "../constants/canvas";
 import { Settings } from "./Settings";
@@ -14,76 +14,98 @@ const Level02: React.FC<Level02Props> = ({
   setGameResult,
   setGameLive,
 }) => {
-  const diameter = settings.radius * 2;
+  const r = settings.radius;
+  const d = settings.radius * 2;
   const j = settings.jiggliness;
-
-  //calculate sum of all x-pixels and y-pixels
-  const xSum = (canvasWidth * (canvasWidth + 1)) / 2;
-  const ySum = (canvasHeight * (canvasHeight + 1)) / 2;
-  console.log(xSum * ySum);
+  const [fill, setFill] = useState(settings.colour1);
+  const [stroke, setStroke] = useState(settings.colour2);
 
   const sketch = (s: P5CanvasInstance) => {
     let x: number;
     let y: number;
-    let pixelCount: number;
-    // let xVisited: number[] = [];
+    const gatesEntered: number[] = [];
+    let colour1 = settings.colour1;
+    let colour2 = settings.colour2;
 
     s.setup = () => {
       s.createCanvas(canvasWidth, canvasHeight);
-      x = s.random(0, s.width);
-      y = s.random(s.height / 2, s.height / 3);
-      pixelCount = (x + y) * diameter;
+      x = Math.round(s.random(0, s.width));
+      y = Math.round(s.random(0, s.height));
     };
 
     s.windowResized = () => {
       s.resizeCanvas(s.windowWidth, s.windowHeight);
-      // s.drawBackground();
-      // s.setupPosition();
     };
 
     s.draw = () => {
-      // s.background(50, 89, 100);
+      // without s.background() the circle produces chemtrails
+      const gateNumber = Math.ceil(s.height / r);
+      const gates = Array.from({ length: gateNumber }, (_, i) => ({
+        start: i === 0 ? 0 : i * r + 1,
+        end: (i + 1) * r,
+      }));
 
       for (let i = 0; i < 6; i++) {
-        s.ellipse(x, y, diameter, diameter);
-        s.fill(settings.colour1);
-        s.stroke(settings.colour2);
+        s.ellipse(x, y, d, d);
+        s.fill(fill);
+        s.stroke(stroke);
         // jiggling
         x = x + s.random(-j, j);
         y = y + s.random(-j, j);
-        // in case it jiggles off screen both axes:
-        if (x < 0) {
-          x = s.width;
-        }
+        //(0,0) is coordinates for top left corner
+        //(s.width, s.height) for bottom right
+        //enter right
         if (x > s.width) {
           x = 0;
         }
-        if (y < 0) {
+        //enter top
+        if (y <= 0) {
           y = s.height;
         }
+        // exit left
+        if (x <= 0) {
+          x = s.width;
+          // make array of object gates where an object == (start, end) and where the length === gateNumber
+          // iterate through gates array
+          // if y value falls between value a and value b in an object of the gates array
+          // then add to set of gatesEntered
+          // when gatesEntered === gates.length, game won
+          const gate = gates.find((gate) => y >= gate.start && y <= gate.end);
+          const gateIndex = gate ? gates.indexOf(gate) : -1;
+          gatesEntered.push(gateIndex);
+          console.log(`Value ${y} is in gate ${gateIndex}`);
+          console.log(`Number of gates entered: ${gatesEntered.length}`);
+          const uniqueGatesEntered: number[] = Array.from(
+            new Set(gatesEntered)
+          );
+          console.log(`List of unique gates entered: ${uniqueGatesEntered}`);
+          if (uniqueGatesEntered.length === gates.length) {
+            setGameResult("won");
+            setGameLive(false);
+          }
+        }
+        //exit bottom
         if (y > s.height) {
           y = 0;
+          // bottomCrossed = bottomCrossed + x;
         }
-        //
+        // movement across canvas
         y = y + 1;
         x = x - 1;
-        pixelCount = pixelCount + (x * diameter + y * diameter);
-        console.log(pixelCount);
-        if (pixelCount > xSum * ySum) {
-          setGameResult("won");
-          setGameLive(false);
-        }
       }
     };
 
-    // s.count = () => {
-    //   const circleX
-    // }
-
     s.mousePressed = () => {
-      let d = s.dist(s.mouseX, s.mouseY, x, y);
-      if (d < diameter) {
-        y = y + 100;
+      let distance = s.dist(s.mouseX, s.mouseY, x, y);
+      if (distance < d) {
+        if (fill === colour1) {
+          setFill(colour2);
+          setStroke(colour1);
+        }
+        if (fill === colour2) {
+          setFill(colour1);
+          setStroke(colour2);
+        }
       }
     };
   };
